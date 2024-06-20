947139150adece1658cecb90d04b973c7cc18e27
SATVIK SHARMA <sat091201@gmail.com>
2024-06-21 00:37:23 +0530

    celerey added railway !!

--- youtube_digest/settings.py
+++ youtube_digest/settings.py
@@ -142,7 +142,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
-CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_BROKER_URL = 'redis://default:mWHmnhnuUVKzsvqzJRPwIUwRMdIptESX@roundhouse.proxy.rlwy.net:25177'
 CELERY_RESULT_BACKEND = 'django-db'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
